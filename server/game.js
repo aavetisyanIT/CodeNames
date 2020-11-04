fa202d86fed5bc48a44b7e54b64493d5c0b2cba3
aavetisyan <aavetisyanit@gmail.com>
2020-11-03 23:59:33 -0600

    Update game.js add board and roleBoard

--- server/game.js
+++ server/game.js
@@ -1,24 +1,17 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const randomWord = require('./random_word.js');
+const shuffle = require('./shuffle.js');
 
 class Game {
 	teamA = [];
 	teamB = [];
 	board = [];
-	roleBoard = [
-		['teamA', 'teamB', 'teamA', 'teamB', 'civilian'],
-		['teamA', 'teamB', 'civilian', 'teamB', 'civilian'],
-		['teamA', 'civilian', 'teamA', 'teamB', 'assassin'],
-		['teamA', 'teamB', 'civilian', 'teamB', 'civilian'],
-		['teamA', 'teamB', 'teamA', 'teamB', 'civilian'],
-	];
+	roleBoard = [];
 
-	constructor(id, board, team, score) {
-		(this.board = board),
-			(this.team = team),
-			(this.score = score),
-			(this.id = id);
+	constructor(gameId, score) {
+		(this.gameId = gameId), (this.score = score);
 	}
 	//adding new team to a game
 	addTeam(newTeam) {
@@ -28,9 +21,46 @@ class Game {
 			this.teamB.push(newTeam.players);
 		}
 	}
-	makeGame(teamA, teamB) {
-		let gameId = (Math.random() + 1).toString(36).slice(2, 18);
-		console.log(`making game with ID: ${gameId}`);
+
+	createBoard() {
+		let board = [];
+		for (let y = 0; y < 5; y++) {
+			let row = [];
+			for (let x = 0; x < 5; x++) {
+				row.push(randomWord());
+			}
+			board.push(row);
+		}
+		return board;
+	}
+
+	roleAssign() {
+		let sampleRoleBoard = [
+			['teamA', 'teamB', 'teamA', 'teamB', 'civilian'],
+			['teamA', 'teamB', 'civilian', 'teamB', 'civilian'],
+			['teamA', 'civilian', 'teamA', 'teamB', 'assassin'],
+			['teamA', 'teamB', 'civilian', 'teamB', 'civilian'],
+			['teamA', 'teamB', 'teamA', 'teamB', 'civilian'],
+		];
+		let newRoleBoard = shuffle(sampleRoleBoard);
+		shuffle(newRoleBoard[0]);
+		shuffle(newRoleBoard[1]);
+		shuffle(newRoleBoard[2]);
+		shuffle(newRoleBoard[3]);
+		shuffle(newRoleBoard[4]);
+		this.roleBoard = newRoleBoard;
+	}
+
+	makeGame(gameId) {
+		//setting gameId
+		this.gameId = gameId;
+		//generating random roll board
+		this.roleAssign();
+		//generate board of ramdom words
+		let board = this.createBoard();
+		this.board = board;
+
+		return this.board;
 	}
 }
 
